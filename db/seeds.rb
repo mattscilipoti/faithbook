2d6530115bd3ee5fb9a96ef651ad9eaa664bf31b
Justin Price <Justin@Justins-MacBook-Pro.local>
2015-08-06 11:28:17 -0400

    images
    removed images from assets/images directory, updated seeds file with remaining images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-bill_profile = Profile.create(full_name: "Bill Johnson", photo_url: "bill.jpg", birth_date: "November 22, 2011", education: "Sunday School", about_me: "Hi, I'm Bill! Praise the Lord!")
+bill_profile = Profile.create(full_name: "Bill Johnson", photo_url: "ppic.jpg", birth_date: "November 22, 2011", education: "Sunday School", about_me: "Hi, I'm Bill! Praise the Lord!")
 
-suzie_comment = Comment.create(full_name: "Suzie Bananas", photo_url: "suzie.jpg", date_time: "August 4, 2015 at 12:00pm", content: "I love you, Bill.", profile_id: 1)
+suzie_comment = Comment.create(full_name: "Suzie Bananas", photo_url: "elisha.jpg", date_time: "August 4, 2015 at 12:00pm", content: "I love you, Bill.", profile_id: 1)
